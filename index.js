dcb2e66261bd4292039fa65b4fa5e2a9c56044b2
Charly Koza <cka@f4-group.com>
2019-09-03 18:14:47 +0200

    Remove fallback to undefined
    Fixes #1

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ module.exports = boobool;
  *
  * @param {String|undefined|null} value the value to parse
  * @param {object} [options]
- * @param {*} [options.defaultValue=undefined] default value when a boolean was not found
+ * @param {*} [options.defaultValue] default value when a boolean was not found
  */
 function boobool(value, options) {
-    const defaultValue = options && options.defaultValue || undefined;
+    const defaultValue = options && options.defaultValue;
     if (value == null) {
         return defaultValue;
     } else {
